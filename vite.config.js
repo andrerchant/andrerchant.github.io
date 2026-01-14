8359c9f76a1df5dd76a60c4af07f582cbc97d05e
Andre Merchant <andremerchant@gmail.com>
2026-01-14 17:42:56 -0600

    update domain name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://andrerchant.github.io/',
+  base: 'https://www.valhalla-repair.com/',
   // Esta línea ayuda a resolver importaciones
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
